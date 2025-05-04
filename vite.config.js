cfa6733a81c2f34c39207f389e6ee2eefa1a2302
Zinah Ghulam <zinahghulam@gmail.com>
2025-05-03 21:09:34 -0400

    Commit changes before deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio/',  // 👈 important for GitHub Pages
+  base: '/portfolio',  // 👈 important for GitHub Pages
   plugins: [react()],
 })
